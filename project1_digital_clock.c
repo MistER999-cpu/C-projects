e4dc1ba9eacea21e064ad0214e3f76326687576d
MistER999-cpu <mohamedrharra22@gmail.com>
2026-01-13 09:49:57 +0100

    Add files via upload

---
+++ project1_digital_clock.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int main(){
+
+     // DIGITAL CLOCK
+
+
+     time_t rawtime = 0;
+     struct tm *pTime = NULL;
+     bool isrunning = true;
+
+
+     printf("DIGITAL CLOCK\n\n");
+
+     while(isrunning){
+
+
+        time(&rawtime);
+
+        pTime = localtime(&rawtime);
+
+        printf("\r%02d:%02d:%02d", (*pTime).tm_hour, (*pTime).tm_min, (*pTime).tm_sec);
+
+
+         sleep(1);
+
+
+     }
+
+     
+
+
+    return 0;
+}
